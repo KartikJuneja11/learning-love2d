ddb557d12e87c1acda01a83cdada74e4078818c9
KartikJuneja11 <154935356+KartikJuneja11@users.noreply.github.com>
2024-06-16 01:32:44 +0530

    Added main and configuration files

---
+++ main.lua
@@ -0,0 +1,15 @@
+function love.draw()
+    love.graphics.print("hello World", 100, 100)
+end
+
+function love.update(dt)
+    
+end
+
+function love.load()
+    
+end
+
+function love.quit()
+    
+end
