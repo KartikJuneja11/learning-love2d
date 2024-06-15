4ffc6282f599aa06eca9c17d3faa48e266581d22
KartikJuneja11 <154935356+KartikJuneja11@users.noreply.github.com>
2024-06-16 03:08:33 +0530

    basic grphics and animations

--- main.lua
+++ main.lua
@@ -1,14 +1,31 @@
+local height = love.graphics.getHeight()
+local width = love.graphics.getWidth()
+currentX = 0
+
 function love.draw()
-    love.graphics.print("Hello World", 100, 100)
+    love.graphics.setColor(1, 0, 0, 1)
+    love.graphics.line(0, 0, 100, 100)
+
+    love.graphics.setColor(0, 0, 1, 1)
+    love.graphics.line(0, height / 2, width, height / 2)
+
+    love.graphics.setColor(0, 1, 0, 0.2)
+    love.graphics.polygon("fill", 
+        {currentX + 0, 100, currentX + 200, 100, 
+        currentX + 200, 300, currentX + 0, 300}
+    )
 end
 
 function love.update(dt)
-    
+    if (currentX < width) then
+        currentX = currentX + 100 * dt
+    else
+        currentX = -200
+    end
 end
 
 function love.load()
-    local newIcon = love.image.newImageData("funny.jpeg")
-    love.window.setIcon(newIcon)
+    love.graphics.setBackgroundColor(255, 255, 255, 255)
 end
 
 function love.quit()
